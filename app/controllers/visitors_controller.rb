2f1ccaf6613751784811761ca0edf6c88adf50fb
Kofi Quaicoe <copernicus74@hotmail.com>
2015-02-03 16:40:39 -0500

    update of application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,6 @@
 class VisitorsController < ApplicationController
   def new
     @owner=Owner.new
+    flash.now[:notice]='Hiyas all'#flash.now is used so the flash message doesnot persist on all pages
   end
 end
